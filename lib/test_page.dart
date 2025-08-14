1ccfcf4d94868cf930f9fc391ff26902336e2fa7
Abladjanva <abladzanovahadica@gmail.com>
2025-08-14 12:34:19 +0300

    commented all

--- lib/test_page.dart
+++ lib/test_page.dart
@@ -1,18 +1,18 @@
-import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/material.dart';
+// import 'package:easy_localization/easy_localization.dart';
+// import 'package:flutter/material.dart';
 
-class TestPage extends StatelessWidget {
-  const TestPage({super.key});
+// class TestPage extends StatelessWidget {
+//   const TestPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: () async {
-        await context.setLocale(const Locale('uz'));
-      }),
-      body: Center(
-        child: Text(context.tr('greeting')),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       floatingActionButton: FloatingActionButton(onPressed: () async {
+//         await context.setLocale(const Locale('uz'));
+//       }),
+//       body: Center(
+//         child: Text(context.tr('greeting')),
+//       ),
+//     );
+//   }
+// }
